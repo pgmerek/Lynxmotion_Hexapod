34b3eb44ae719bd23bba1f720faa69d7ef9728bb
Patrick Gmerek <pgmerek@pdx.edu>
2018-10-10 15:14:15 -0700

    Played with opencv a bit

--- exercises/opencv.py
+++ exercises/opencv.py
@@ -1,2 +1,17 @@
+"""
+Simple program to get my feet wet with openCV.
+"""
+
 import cv2 as cv
+import numpy as np
+
 print(cv.__version__)
+
+# Load image
+img = cv.imread('cat.jpg', 0)
+
+# Show image
+cv.imshow('Cat Image', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
