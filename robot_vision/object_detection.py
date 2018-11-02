976df61cf60dcdb90e14f054cbf8544c6c82b5ba
Emma <emma25@pdx.edu>
2018-11-01 21:46:54 -0700

    Added a pi cam and a laptop cam fucntion

--- robot_vision/object_detection.py
+++ robot_vision/object_detection.py
@@ -6,26 +6,30 @@ import numpy as np
 import imutils
 import time
 import cv2
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+# from picamera.array import PiRGBArray
+# from picamera import PiCamera
 
 
-class my_object:
-    def __init___(self, object_type, depth, width):
+class MyObject:
+    def __init__(self, frame, object_type, size, location):
+        self.frame = frame
         self.object_type = object_type
-        self.depth = depth
-        self.width = width
+        self.size = size
+        self.location = location
 
 
-MAX_SIZE = 50
-MIN_SIZE = 10
+MAX_RADIUS = 50
+MIN_RADIUS = 0.5
+SMALL = 10
+MEDIUM = 30
+LARGE = 50
 
 
-# define the lower and upper boundaries of the colors in the HSV color space
+# Define the lower and upper boundaries of the colors in the HSV color space
 lower = {'red': (166, 84, 141), 'green': (66, 122, 129), 'blue': (97, 100, 117), 'yellow': (23, 59, 119)}  # assign new item lower['blue'] = (93, 10, 0)
 upper = {'red': (186, 255, 255), 'green': (86, 255, 255), 'blue': (117, 255, 255), 'yellow': (54, 255, 255)}
 
-# define standard colors for circle around the object
+# Define standard colors for circle around the object
 colors = {'red': (0, 0, 255), 'green': (0, 255, 0), 'blue': (255, 0, 0), 'yellow': (0, 255, 217),
           'orange': (0, 140, 255)}
 
@@ -51,8 +55,6 @@ def detect_color(image):
         # (x, y) center of the ball
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
                                 cv2.CHAIN_APPROX_SIMPLE)[-2]
-        center = None
-
         # only proceed if at least one contour was found
         if len(cnts) > 0:
             # find the largest contour in the mask, then use
@@ -71,38 +73,48 @@ def detect_color(image):
                 cv2.putText(frame, key, (int(x - radius), int(y - radius)), cv2.FONT_HERSHEY_SIMPLEX, 0.6,
                             colors[key], 2)
 
-                image = my_object()
-                image.depth = radius
-                image.width = center
-                image.object_type = key
+                detected_object = MyObject(frame, key, radius, center)
+                return detected_object
 
-                return image
-
-    no_image = my_object()
-    no_image.depth = "non"
-    no_image.object_type = "non"
-    no_image.width = "non"
+    no_image = MyObject("None", "None", "None")
     return no_image
-    
-
-# camera = cv2.VideoCapture(0)
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-
-time.sleep(0.1)
 
 
 counter = 1
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    image = frame.array
-    color = detect_color(image)
-    counter += 1
-    print (counter, "I see a ", color.object_type, "object, that is ", color.depth, " in size and is located at ", color.width)
-    cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
-    rawCapture.truncate(0)
-cv2.destroyAllWindows()
+'''def raspi_camera():
+    # camera = cv2.VideoCapture(0)
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 32
+    raw_capture = PiRGBArray(camera, size=(640, 480))
+
+    time.sleep(0.1)
+
+    for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+        image = frame.array
+        color = detect_color(image)
+        counter += 1
+        print(counter, "I see a ", color.object_type, "object, that is ", color.size, " in size and is located at ", color.location)
+        cv2.imshow("Frame", image)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+        raw_capture.truncate(0)
+    cv2.destroyAllWindows()
+'''
+
+
+def laptop_camera():
+    camera = cv2.VideoCapture(0)
+    while True:
+        (grabbed, frame) = camera.read()
+        color = detect_color(frame)
+        print(counter, "I see a ", color.object_type, "object, that is ", color.size, " in size and is located at ", color.location)
+        cv2.imshow("Frame", color.frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+    camera.release()
+    cv2.destroyAllWindows()
+
+
