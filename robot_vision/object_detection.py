3aab2630956cec552b4c43915e22db68db757d14
Emma <emma25@pdx.edu>
2018-11-01 22:24:26 -0700

    Added global paramaters for frame, size, and location

--- robot_vision/object_detection.py
+++ robot_vision/object_detection.py
@@ -18,15 +18,17 @@ class MyObject:
         self.location = location
 
 
+FRAME_SIZE = 600
 MAX_RADIUS = 50
 MIN_RADIUS = 0.5
-SMALL = 10
-MEDIUM = 30
-LARGE = 50
+LEFT = FRAME_SIZE/3
+RIGHT = 2 * FRAME_SIZE/3
+SMALL = (MAX_RADIUS * MAX_RADIUS/3) * np.pi
+LARGE = 2 * (MAX_RADIUS* MIN_RADIUS/3) * np.pi
 
 
 # Define the lower and upper boundaries of the colors in the HSV color space
-lower = {'red': (166, 84, 141), 'green': (66, 122, 129), 'blue': (97, 100, 117), 'yellow': (23, 59, 119)}  # assign new item lower['blue'] = (93, 10, 0)
+lower = {'red': (166, 84, 141), 'green': (66, 122, 129), 'blue': (97, 100, 117), 'yellow': (23, 59, 119)}
 upper = {'red': (186, 255, 255), 'green': (86, 255, 255), 'blue': (117, 255, 255), 'yellow': (54, 255, 255)}
 
 # Define standard colors for circle around the object
@@ -35,16 +37,20 @@ colors = {'red': (0, 0, 255), 'green': (0, 255, 0), 'blue': (255, 0, 0), 'yellow
 
 
 def detect_color(image):
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    frame = imutils.resize(image, width=600)
+    """
+    Detect objects by color in image
+    :param image: image to detect objects from
+    :return: returns the processed image, color, size, and location
+    """
+    # Resize the frame, blur it, and convert it to the HSV color space
+    frame = imutils.resize(image, width=FRAME_SIZE)
 
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    # for each color in dictionary check object in frame
+    # For each color in dictionary check object in frame
     for key, value in upper.items():
-        # construct a mask for the color from dictionary`1, then perform
-        # a series of dilations and erosions to remove any small
+        # Construct a mask for the color from dictionary`1, then perform
+        # A series of dilations and erosions to remove any small
         # blobs left in the mask
         kernel = np.ones((9, 9), np.uint8)
         mask = cv2.inRange(hsv, lower[key], upper[key])
@@ -53,35 +59,50 @@ def detect_color(image):
 
         # find contours in the mask and initialize the current
         # (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+        contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)[-2]
         # only proceed if at least one contour was found
-        if len(cnts) > 0:
+        if len(contours) > 0:
             # find the largest contour in the mask, then use
             # it to compute the minimum enclosing circle and
             # centroid
-            c = max(cnts, key=cv2.contourArea)
+            c = max(contours, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
             # only proceed if the radius meets a minimum size. Correct this value for your obect's size
-            if 0.5 < radius < 50:
+            if MIN_RADIUS < radius < MAX_RADIUS:
                 # draw the circle and centroid on the frame,
                 # then update the list of tracked points
                 cv2.circle(frame, (int(x), int(y)), int(radius), colors[key], 2)
                 cv2.putText(frame, key, (int(x - radius), int(y - radius)), cv2.FONT_HERSHEY_SIMPLEX, 0.6,
                             colors[key], 2)
 
-                detected_object = MyObject(frame, key, radius, center)
+                object_size = np.pi * radius * radius
+                if object_size <= SMALL:
+                    object_size = "small"
+                elif object_size >= LARGE:
+                    object_size = "large"
+                else:
+                    object_size = "medium"
+
+                object_location = "Middle"
+                if center[0] <= LEFT:
+                    object_location = "left"
+                elif center[0] >= RIGHT:
+                    object_location = "right"
+                detected_object = MyObject(frame, key, object_size, object_location)
                 return detected_object
 
-    no_image = MyObject("None", "None", "None")
+    no_image = MyObject(frame, "None", "None", "None")
     return no_image
 
 
-counter = 1
 '''def raspi_camera():
+"""
+Use this function when running this on the pi
+"""
     # camera = cv2.VideoCapture(0)
     camera = PiCamera()
     camera.resolution = (640, 480)
@@ -90,12 +111,13 @@ counter = 1
 
     time.sleep(0.1)
 
+    counter = 1
     for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
         image = frame.array
         color = detect_color(image)
         counter += 1
         print(counter, "I see a ", color.object_type, "object, that is ", color.size, " in size and is located at ", color.location)
-        cv2.imshow("Frame", image)
+        cv2.imshow("Frame", color.frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
@@ -105,11 +127,18 @@ counter = 1
 
 
 def laptop_camera():
+    """
+    Use this function when running on a laptop
+    :return: nothing
+    """
+    counter = 1
     camera = cv2.VideoCapture(0)
+    # Grab frame and process until user presses "q"
     while True:
         (grabbed, frame) = camera.read()
         color = detect_color(frame)
         print(counter, "I see a ", color.object_type, "object, that is ", color.size, " in size and is located at ", color.location)
+        counter += 1
         cv2.imshow("Frame", color.frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
@@ -118,3 +147,4 @@ def laptop_camera():
     cv2.destroyAllWindows()
 
 
+laptop_camera()
