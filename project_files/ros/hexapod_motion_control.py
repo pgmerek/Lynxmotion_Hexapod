cb6a77b8e2b4801ffa0aa6df885b50902b283496
charlesstoll <charlesrstoll@gmail.com>
2018-11-26 14:53:28 -0800

    add torso controls and finished hex controls
    added all of the torso commands and finished all of the hexapod
    commands. they now are a single node but have separate callbacks for
    each. torso_motion subscribes to torso_command and publishes to
    torso_command_finished while the hexapod motion is subscribed to
    motion_comand and publishes to motion_command_finished

--- project_files/ros/hexapod_motion_control.py
+++ project_files/ros/hexapod_motion_control.py
@@ -35,42 +35,117 @@ rot = Rotator(0, pwm_40, 9)
 
 torso = Robot_Torso(r, l, rot)
 
+global finish_hex_pub
+global torso_done
+global hex_done
+torso_done = 0
+hex_done = 0
 
-global finish_command_pub
-global done
-done = 0
-
-def do_command(data):
-    global done
-    global finish_command_pub
+def do_hex_walker_command(data):
+    global hex_done
+    global finish_hex_pub
     rospy.loginfo(rospy.get_caller_id() + ' I heard %s', data.data)
     commandlist = data.data.split()
+    
+    
     if commandlist[0] == "walk":
         if len(commandlist) != 3:
             assert "incorrect number of arguments for walk"
-        print("walking 4 steps forward")
-        hex_walker.walk(4,0)
+        num_steps = int(commandlist[1])
+        direction = int(commandlist[2])
+        print("walking " + str(num_steps) + " steps forward in direction " + str(direction))
+        hex_walker.walk(num_steps, direction)
+    
+    
     if commandlist[0] == "rotate":
         if len(commandlist) != 3:
             assert "incorrect number of arguments for rotate"
-        hex_walker.rotate(1, LEFT)
+        num_steps = int(commandlist[1])
+        if(commandlist[2] == "left"):
+            direction = LEFT
+        else:
+            direction = RIGHT
+        print("rotating " + str(num_steps) + commandlist[2])
+        hex_walker.rotate(num_steps, direction)
+    
+    
     if commandlist[0] == "leg_wave":
         if len(commandlist) != 3:
-            assert "incorrect number of arguments for dance"
-        hex_walker.leg_wave(LEFT, .1, 4)
+            assert "incorrect number of arguments for leg_wave"
+        num_times = int(commandlist[1])
+        if(commandlist[2] == "left"):
+            direction = LEFT
+        else:
+            direction = RIGHT
+        print("leg waving " + str(num_times) + " in the direction " + commandlist[2])
+        hex_walker.leg_wave(direction, .1, num_times)
+    
+    
     if commandlist[0] == "bounce":
         if len(commandlist) != 3:
+            assert "incorrect number of arguments for bounce"
+        num_times = commandlist[1]
+        wait_time = float(commandlist[2])
+        hex_walker.bounce(wait_time, num_times)
+
+
+    hex_done = hex_done + 1
+    finish_hex_pub.publish(hex_done)    
+
+def do_torso_command(data):
+    global torso_done
+    global finish_torso_pub
+    rospy.loginfo(rospy.get_caller_id() + ' I heard %s', data.data)
+    commandlist = data.data.split()
+    
+    
+    if commandlist[0] == "wave":
+        if len(commandlist) != 3:
             assert "incorrect number of arguments for wave"
-        hex_walker.bounce(.3, 4)
-    done = done + 1
-    finish_command_pub.publish(done)    
+        num_times = int(commandlist[1])
+        direction = int(commandlist[2])
+        print("waving " + str(num_times) + " times in direction " + str(direction))
+        torso.wave(direction, num_times)
+
+
+    if commandlist[0] == "king_kong":
+        if len(commandlist) != 3:
+            assert "incorrect number of arguments for king_kong"
+        num_times = int(commandlist[1])
+        direction = int(commandlist[2])
+        print("king_kong-ing " + str(num_times) " times in direction " + str(direction)
+        torso.king_kong(direction, num_times)
+    
+        
+    if commandlist[0] == "monkey":
+        if len(commandlist) != 2:
+            assert "incorrect number of arguments for monkey"
+        num_times = int(commandlist[1])
+        print("monkeying " + str(num_times) + " times"
+        torso.monkey(num_times)
+    
+        
+    if commandlist[0] == "handshake":
+        if len(commandlist) != 3:
+            assert "incorrect number of arguments for handshake"
+        num_times = int(commandlist[1])
+        direction = int(commandlist[2])
+        torso.hand_shake(direction, num_times) 
+    
+        
+    torso_done = torso_done + 1
+    finish_torso_pub.publish(torso_done)    
+
 
 def node_setup():
-    global finish_command_pub
+    global finish_hex_pub
     rospy.init_node('hexapod_motion_controller')
-    rospy.Subscriber('motion_command', String, do_command)
-    finish_command_pub = rospy.Publisher('motion_command_finished', Int32, queue_size = 1) 
-    finish_command_pub.publish(0)
+    rospy.Subscriber('motion_command', String, do_hex_walker_command)
+    rospy.Subscriber('torso_command', String, do_torso_command)
+    finish_hex_pub = rospy.Publisher('motion_command_finished', Int32, queue_size = 1) 
+    finish_torso_pub = rospy.Publisher('torso_command_finished', Int32, queue_size = 1)
+    finish_hex_pub.publish(0)
+    finish_torso_pub.publish(0)
     rospy.spin()
 
 if __name__ == '__main__':
