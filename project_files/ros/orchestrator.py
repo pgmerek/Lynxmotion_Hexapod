032d309cbd12648dca3cab5aff245c180e846b29
pgmerek <pgmerek@pdx.edu>
2018-11-26 23:18:20 -0800

    Initial, untested version. Now running with infinite percent less syntax error.

--- project_files/ros/orchestrator.py
+++ project_files/ros/orchestrator.py
@@ -18,6 +18,8 @@ global play_started
 global play_counter
 global play_lines
 global play_motions
+global our_turn
+global turing_done
 # Globals for sending commands
 global torso_command   # Stores the torso command that will be published
 global motion_command   # Stores the walker command that will be published
@@ -30,7 +32,7 @@ global vision_return    # Stores what the eyes node saw
 global tts_done    # Increments as more sentences are said
 global tts_command # Stores the talk command that will be published
 global stt_done    # Increments as more sentences are heard
-global stt_command
+global stt_command 
 
 # Initialize publishers
 torso_command_publisher = rospy.Publisher('torso_command', String, queue_size=1)
@@ -38,6 +40,7 @@ motion_command_publisher = rospy.Publisher('torso_command', String, queue_size=1
 vision_command_publisher = rospy.Publisher('vision_command', Int32, queue_size=1)
 tts_command_publisher = rospy.Publisher('talk_command', String, queue_size=1)
 sst_command_publisher = rospy.Publisher('record_command', Int32, queue_size=1)
+feynman_command_publisher = rospy.Publisher('feynman_command', Int32, queue_size=1)
 
 
 # main has intialization values and function calls
@@ -49,10 +52,12 @@ def main():
     global motion_done
     global send_torso_command
     global send_motion_command
+    global our_turn
     global play_started
     global play_counter
     global play_motions
     global play_lines
+    global turing_done
     global enable_vision
     global vision_return
     global tts_done     # tts is text to speech
@@ -67,6 +72,8 @@ def main():
     play_counter = 0
     play_lines = []
     play_motions = []
+    our_turn = 0
+    turing_done = 0
     # Motion section
     torso_command = ""  # Empty strings evaluate False in Python
     motion_command = ""
@@ -82,19 +89,17 @@ def main():
     tts_command = ""
     stt_done = 0
     stt_command = ""
-    record_command = 0
+    record_command = ""
     record_command_done = 0
 
     # Get the lines, text files must be in same directory as orchestrator.py
     with open(getcwd() + '/lines.txt', 'r') as file:
         line = file.read()
         play_lines.append(line)
-        print(line)
     # Get the motions
     with open(getcwd() + '/motions.txt', 'r') as file:
         motion = file.read()
         play_motions.append(motion)
-        print(motion)
     
     # Call orchestrator but capture exception if thrown
     try:
@@ -113,10 +118,20 @@ def orchestrator():
     global send_torso_command
     global send_motion_command
     global play_started
+    global play_counter
+    global play_motions
+    global play_lines
+    global our_turn
     global enable_vision
     global vision_return
     global talk_done
     global talk_command
+    global tts_done
+    global tts_command
+    global stt_done
+    global stt_command
+    global record_command
+    global record_command_done
 
     # Intitialize node
     rospy.init_node('orchestrator', anonymous=True)
@@ -128,34 +143,36 @@ def orchestrator():
     rospy.Subscriber('torso_command_finished', Int32, torso_command_finished_callback)
     rospy.Subscriber('vision_finished', Int32, vision_finished_callback)
     rospy.Subscriber('vision_return', String, vision_return_callback)
-    rospy.Subscriber('talk_done', Int32, talk_done_callback)
+    rospy.Subscriber('talk_command_finished', Int32, talk_command_finished_callback)
     rospy.Subscriber('record_command_finished', Int32, record_command_finished_callback)
     rospy.Subscriber('speech_command_finished', Int32, speech_command_finished_callback)
+    rospy.Subscriber('turing_done', Int32, turing_done_callback)
 
 
     while not rospy.is_shutdown():
         if play_started:    # If play is started, go to an entirely different function
             execute_play()
-        # If we have a motion command and we are cleared to send it, send it
-        if motion_command and send_motion_command:
-            motion_command_publisher.publish(motion_command)
-            motion_command = "" # Reset flag and motion command
-            send_motion_command = 0
+        else:
+            # If we have a motion command and we are cleared to send it, send it
+            if motion_command and send_motion_command:
+                motion_command_publisher.publish(motion_command)
+                motion_command = "" # Reset flag and motion command
+                send_motion_command = 0
 
-        # If we have a torso command and we are cleared to send it, send it
-        if torso_command and send_torso_command:  
-            torso_command_publisher.publish(torso_command)
-            torso_command = "" # Reset flag and torso command
-            send_torso_command = 0
+            # If we have a torso command and we are cleared to send it, send it
+            if torso_command and send_torso_command:  
+                torso_command_publisher.publish(torso_command)
+                torso_command = "" # Reset flag and torso command
+                send_torso_command = 0
 
-        # If we have a sentence to say and we are allowed to say it, send it
-        if talk_command and send_sentence:
-            talk_command_publisher.publish(talk_command)
-            talk_command = ""   # Reset sentence
-            send_sentence = 0
+            # If we have a sentence to say and we are allowed to say it, send it
+            if tts_command and send_sentence:
+                talk_command_publisher.publish(tts_command)
+                tts_command = ""   # Reset sentence
+                send_sentence = 0
 
-        # Vision is blocking
-        vision_command_publisher.publish(enable_vision)
+            # Vision is blocking
+            vision_command_publisher.publish(enable_vision)
 
         refresh_rate.sleep()
 
@@ -164,19 +181,86 @@ def orchestrator():
 
 
 # Function to execute the play
-#def execute_play(file_path):
-
-# Callback function for talk done
-def talk_done_callback(data):
-    global talk_done
+def execute_play():
+    global play_started
+    global play_counter
+    global play_motions
+    global play_lines
+    global our_turn # True if it's our turn to speak, move, or both
+    global send_motion_command
     global send_sentence
 
-    if data.data == talk_done:  # Do nothing unless talk_done increments from the talk node
+    # If it's our turn 
+    if our_turn:
+        # If we can talk and move
+        if send_sentence and send_motion_command:
+            if play_lines[play_counter] != "WAIT_FOR_TURING":   # Speak as long as the line isn't "WAIT_FOR_TURING" 
+                talk_command_publisher.publish(play_lines[play_counter])    # Publish the line
+                motion_command_publisher.publish(play_motions[play_counter])    # Publish the motion
+                print("Sending the line \"{0}\" and the motion \"{1}\".".format(play_lines[play_counter], play_motions[play_counter]))
+                play_counter += 1 # Increment counter
+                send_sentence = 0   # Reset the flags for tts and motion
+                send_motion_command = 0
+                if play_counter > 13:   # At end of play, stop and reset for next play
+                    our_turn = 0
+                    play_started = 0
+                    play_counter = 0
+                    print("And that concludes act I of our play.")
+            else:
+                our_turn = 0    # Stop talking since now it's Turing's turn
+                play_counter += 1 # Increment counter past the wait line. We'll still be blocked because our_turn is False
+                feynman_command_publisher.publish(1)    # Publish a one to tell Turing it's his turn                
+                print("Done talking and moving for now. Waiting for Turing.")
+    else:
+        feynman_command_publisher.publish(1)    # Publish a one to tell Turing it's his turn                
+        print("Waiting for Turing")
+
+
+# Callback function for Turing done
+def turing_done_callback(data):
+    global our_turn
+    global turing_done
+
+    if data.data == turing_done:
+        pass
+    else:
+        our_turn = 1
+        turing_done = data.data
+        print("Turing said he's done with his part.")
+
+# Callback function for speech 
+def speech_command_finished_callback(data):
+    global sst_done
+
+    if data.data == sst_done:
+        pass
+    else:
+        sst_done = data.data
+        print("Done converting speech to text.")
+
+
+# Callback function for record done
+def record_command_finished_callback(data):
+    global record_command_done
+
+    if data.data == record_command_done:  # Do nothing unless record_done increments from the talk node
+        pass
+    else:
+        record_command_done = data.data
+        print("Record is finished.")
+
+
+# Callback function for talk done
+def talk_command_finished_callback(data):
+    global tts_done
+    global send_sentence
+
+    if data.data == tts_done:  # Do nothing unless talk_done increments from the talk node
         pass
     else:
         send_sentence = 1
-        talk_done = data.data
-        print("Talk is finished.")
+        tts_done = data.data
+        print("Done converting text to speech.")
 
 
 # Callback function for vision return
